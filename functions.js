1994cd800b35ec60ceb29fd2ba5c01201d5daedb
will1166 <will1166@algonquinlive.com>
2026-05-30 23:09:47 -0400

    Adds comments and creates order function

--- functions.js
+++ functions.js
@@ -1,9 +1,18 @@
-// 2. list function
+// 2. list function to generate an HTML string list of clients from an array
 const list = (clientArray) => {
+    // map() iterates through each client and return an array of list items strings
     return clientArray.map(client => `<li class="list-group-item d-flex justify-content-between" data-index="${client.index}">
   ${client.name}
   <strong>$ ${client.balance}</strong>
-</li>`).join("");
+</li>`).join(""); // convert to a string
 };
- 
-// 3.
+
+ // 3. order function to sort an array of clients based on a specific property
+const order = (clientArray, property) => {
+    // sort() compares two elements
+    return clientArray.sort((a, b) => {
+        if (a[property] < b[property]) return -1; // a before b
+        if (a[property] > b[property]) return 1; // b before a
+        return 0; // keeps original order if values are equal
+    });
+};
